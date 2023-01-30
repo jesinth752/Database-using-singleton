f9e3be0934b33e711277488cd582728a419b65c1
jesinth752 <123439338+jesinth752@users.noreply.github.com>
2023-01-30 15:21:23 +0530

    Update Database.java

--- src/main/java/org/example/Database.java
+++ src/main/java/org/example/Database.java
@@ -8,17 +8,6 @@ class Dbconnection
 {
     private static final Logger LOGGER = Logger.getLogger("InfoLogging");
 
-   int status=0;
-package org.example;
-
-
-import java.util.Scanner;
-import java.util.logging.Logger;
-
-class Dbconnection
-{
-    private static final Logger LOGGER = Logger.getLogger("InfoLogging");
-
     int status=0;
 
      static Dbconnection connector;
@@ -42,10 +31,10 @@ class Dbconnection
     {
         if(status==1)
         {
-            LOGGER.info("Already  connection {} is exists"+connector);
+            LOGGER.info("Already  connection {} is exists",connector);
         }
         else {
-            LOGGER.info("connected to {} "+connector);
+            LOGGER.info("connected to {} ",connector);
         }
         status=1;
 
@@ -103,97 +92,6 @@ public class Database {
                 case 3 -> db.closeconnection();
                 case 4 -> run=1;
                 default ->LOGGER.info(" Invalid choice");
-
-            }
-
-        }
-
-
-
-    }
-}
-    Dbconnection connector=null;
-
-
-    private Dbconnection()
-    {
-    }
-    public static Dbconnection getInstance()
-    {
-        if(connector==null)
-        {
-
-            connector=new Dbconnection();
-        }
-
-
-        return connector;
-    }
-    void newconnection()
-    {
-        if(status==1)
-        {
-            LOGGER.info("Already  connection {} is exists",connector);
-        }
-        else {
-            LOGGER.info("connected to {} ",connector);
-        }
-        status=1;
-
-
-    }
-    void checkconnection()
-    {
-        if(status==1)
-        {
-            LOGGER.info("connection status:ON ");
-
-        }
-        else
-        {
-            LOGGER.info("connection status:OFF ");
-        }
-
-    }
-    void closeconnection()
-    {
-
-
-         connector=null;
-        if(status==1)
-        {
-            status=0;
-            LOGGER.info(" Connection is closed");
-        }
-        else
-        {
-
-            LOGGER.info("NO active Connection");
-        }
-
-
-    }
-}
-public class Database {
-    private static final Logger LOGGER = Logger.getLogger("InfoLogging");
-    public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
-
-        int opt;
-        int run=0;
-        while(run==0) {
-            Dbconnection db=Dbconnection.getInstance();
-
-            LOGGER.info("1.New Connection \n2.check Connection Status \n3.close Connection \n4.exit ");
-            LOGGER.info("Enter option:");
-
-            opt= input.nextInt();
-            switch (opt) {
-                case 1 -> db.newconnection();
-                case 2 -> db.checkconnection();
-                case 3 -> db.closeconnection();
-                case 4 -> run=1;
-                 default ->LOGGER.info(" Invalid choice");
 
             }
 
